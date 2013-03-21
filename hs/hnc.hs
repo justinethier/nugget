45c9c788250ccedfb892335036f55a384ed2e63e
Justin Ethier <justin.ethier@gmail.com>
2013-03-21 17:09:02 -0400

    Added an AST stub

--- hs/hnc.hs
+++ hs/hnc.hs
@@ -10,10 +10,12 @@ An experimental port of the 90 minute scheme compiler to Haskell.
 -}
 
 module Main where
-import qualified Language.Scheme.Core      -- Scheme Interpreter
-import qualified Language.Scheme.Types     -- Scheme data types
-import qualified Language.Scheme.Variables -- Scheme variable operations
+import qualified Language.Scheme.Core as LSC
+import qualified Language.Scheme.Primitives as LSP
+import Language.Scheme.Types     -- Scheme data types
+-- import qualified Language.Scheme.Variables -- Scheme variable operations
 import Control.Monad.Error
+import qualified System.Exit
 import System.IO
 import System.Environment
 
@@ -22,10 +24,28 @@ main = do
     args <- getArgs
     case (null args) of
         True -> showBanner
-        _ -> compileFile $ head args
+        _ -> do compileFile $ head args
+--            result <- (LSC.runIOThrows $ liftM show $ compileFile $ head args)
+--            case result of
+--                Just errMsg -> putStrLn errMsg
+--                _ -> putStrLn "TODO"
 
 showBanner :: IO ()
-showBanner = putStrLn "TODO: banner"
+showBanner = putStrLn "Usage: hnc filename"
 
 compileFile :: String -> IO ()
-compileFile filename = putStrLn $ "TODO: " ++ filename
+compileFile filename = do
+    ast <- loadFile filename
+    System.Exit.exitSuccess
+
+loadFile :: String -> IO [LispVal]
+loadFile filename = do
+    result <- runErrorT $ LSP.load filename
+    case result of
+        Left err -> do
+            putStrLn $ show err
+            System.Exit.exitFailure
+        Right ast -> do
+            putStrLn "-------------------------- AST:"
+            putStrLn $ show ast
+            return $ ast
