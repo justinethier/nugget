2e0c7c51de03d888cd368559a80f185a4a157b0c
Justin Ethier <justin.ethier@gmail.com>
2013-04-25 13:39:06 -0400

    Initial file

---
+++ run-tests.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+nsc 90-min-scc/test1.scm
+90-min-scc/test1
+nsc 90-min-scc/test2.scm
+90-min-scc/test2
+nsc 90-min-scc/test3.scm
+90-min-scc/test3
+nsc tests/00-simplest.scm
+tests/00-simplest
+nsc tests/01-simple.scm
+tests/01-simple
+nsc tests/02-basic-function.scm
+tests/02-basic-function
+nsc tests/03-top-level-test1.scm
+tests/03-top-level-test1
