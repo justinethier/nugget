27d2c43384f29f0e7cdd3aff2d7c306618094ffa
Justin Ethier <justin.ethier@gmail.com>
2013-04-26 17:30:51 -0400

    Added simple test case; other one was covered

--- tests/04-macro-full.scm
+++
@@ -1,7 +0,0 @@
-(begin
-  (let ((x 1)
-        (y 2)
-        (z 4))
-    (display (+ x (- z y)))
-    (+ y z))
-)
---
+++ tests/04-macro-simple.scm
@@ -0,0 +1,14 @@
+; A simple use of macros
+; TODO: this is broken when begin is left in... WTF?
+;;(begin
+;;  (let ((x 1)
+;;        (y 2)
+;;        (z 4))
+;;    (display (+ x (- z y)))
+;;    (+ y z))
+;;)
+;;
+;; ; TODO: two more examples:
+;; (begin 48 (let ((x 90)) (+ x x))) ; Another closure issue?
+;; (begin 48 90) ; Works
+(begin 48 ((lambda () 90)))
