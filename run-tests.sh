d58064a1c83b8819ee6572dc59db18be2c6965c8
Justin Ethier <justin.ethier@gmail.com>
2013-04-26 17:23:13 -0400

    Print output of 90-min-scc and nsc
    Refactored the code to support compiling and running each test case using both compilers, to verify expected output.

--- run-tests.sh
+++ run-tests.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
-nsc 90-min-scc/test1.scm
-90-min-scc/test1
-nsc 90-min-scc/test2.scm
-90-min-scc/test2
-nsc 90-min-scc/test3.scm
-90-min-scc/test3
-nsc tests/00-simplest.scm
-tests/00-simplest
-nsc tests/01-simple.scm
-tests/01-simple
-nsc tests/02-basic-function.scm
-tests/02-basic-function
-nsc tests/03-top-level-test1.scm
-tests/03-top-level-test1
+NMS=90-min-scc/90-min-scc.scm
+NMSDIR=90-min-scc
+
+function testScm {
+    $NMS $1.scm > /dev/null && gcc $1.c && ./a.out
+    nsc $1.scm
+    $1
+    echo ""
+}
+
+testScm $NMSDIR/test1
+testScm $NMSDIR/test2
+testScm $NMSDIR/test3
+
+testScm tests/00-simplest
+testScm tests/01-simple
+testScm tests/02-basic-function
+testScm tests/03-top-level-test1
+
+rm -f a.out
