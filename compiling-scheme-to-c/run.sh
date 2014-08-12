d9aa667ab62031b56e2d483ba613349920d16091
Justin Ethier <justin.ethier@gmail.com>
2014-08-11 21:45:51 -0400

    WIP

--- compiling-scheme-to-c/run.sh
+++ compiling-scheme-to-c/run.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-
-# does not work
 echo "(display ((lambda (x) (* x x)) 10))" | huski scheme-to-c.scm > 100.c && gcc 100.c -o 100
 
 # WIP
--- compiling-scheme-to-c/test.scm
+++ compiling-scheme-to-c/test.scm
@@ -1,6 +1,5 @@
-(+ 1 2)
-
-;(define n 3)
-;(define (comp x)
-;    (+ 1 2 3 x n))
-;(comp 10)
+;(+ 1 2)
+;
+(define n 2)
+(define (comp x) (+ x n))
+(display (comp 10))
