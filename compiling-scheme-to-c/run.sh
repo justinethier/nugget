3030b58de3045854cbaa52b7ffdbb60df86c4ae6
Justin Ethier <justin.ethier@gmail.com>
2014-08-12 13:09:54 -0400

    Custom compiler, start by using read-all for input

--- compiling-scheme-to-c/run.sh
+++ compiling-scheme-to-c/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "(display ((lambda (x) (* x x)) 10))" | huski scheme-to-c.scm > 100.c && gcc 100.c -o 100
+echo "(display ((lambda (x) (* x x)) 10))" | huski jae.scm > 100.c && gcc 100.c -o 100
 
 # WIP
-huski scheme-to-c.scm < tests/square.scm > out.c
+huski jae.scm < tests/square.scm > out.c
--- compiling-scheme-to-c/tests/square.scm
+++ compiling-scheme-to-c/tests/square.scm
@@ -1,2 +1,7 @@
-(let ((x 10) (y 20) (square (lambda (x) (* x x)))) (begin (display (square x)) (display (square y))))
 ;(begin (set! x 10) (set! y 20) (set! square (lambda (x) (* x x))) (display (square x)) (display (square y)))
+(let ((x 10) 
+      (y 20) 
+      (square (lambda (x) (* x x)))) 
+  (begin 
+    (display (square x)) 
+    (display (square y))))
