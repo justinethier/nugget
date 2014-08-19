bd24adb0829ba07b1649cf3b93a89a843b987f1d
Justin Ethier <justin.ethier@gmail.com>
2014-08-19 17:42:23 -0400

    Cleaning up directory

--- nugget/run.sh
+++ nugget/run.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-echo "(display ((lambda (x) (* x x)) 10))" | huski jae.scm > 100.c && gcc 100.c -o 100
+#echo "(display ((lambda (x) (* x x)) 10))" | huski compiler.scm > 100.c && gcc 100.c -o 100
 
-# WIP
-huski jae.scm < tests/square.scm > out.c
+if [[ $# -eq 0 ]] ; then
+    echo 'usage: run.sh file.scm'
+    exit 0
+fi
+
+huski compiler.scm < $1 > tmp.c && gcc tmp.c && ./a.out
