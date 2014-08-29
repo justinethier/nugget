7a566e478b33c1a610725afe7768b7cf1979be5a
Justin Ethier <justin.ethier@gmail.com>
2014-08-29 12:21:01 -0400

    Added future test cases

--- nugget/tmp.scm
+++ nugget/tmp.scm
@@ -1,2 +1,14 @@
-;(lambda (x) (tautology (rewrite x) 1 2))
-#t
+; Adding examples here as one-line baby steps for the compiler
+;#t ; Bare minimum 
+((lambda (x) x) (cons #t #f))
+;((lambda (x) x) (cons #t (cons #t #f)))
+; (display #t)
+; (display 1)
+; (display '(1 2 3)
+; (display '(a b c)
+; TODO: actual closure demonstration
+; TODO: variable demonstration
+; TODO: if demonstration, maybe (if #t (display #t) (display #f))
+; TODO: if demonstration with a variable
+; TODO: loop demonstration
+;(lambda (x) (tautology (rewrite x) 1 2)) ; Incomplete snippet from boyer
