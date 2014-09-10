a90f6a5b91bf324bc6177ddcb196961783a3ded1
Justin Ethier <justin.ethier@gmail.com>
2014-09-10 13:26:59 -0400

    Added more TODO items

--- nugget/tmp.scm
+++ nugget/tmp.scm
@@ -1,15 +1,18 @@
 ; Adding examples here as one-line baby steps for the compiler
-;#t ; Bare minimum 
-((lambda (x) x) (cons #t #f))
-;((lambda (x) x) (cons #t (cons #t #f)))
-; (display #t)
-; (display #f)
+#t ; Bare minimum 
+(display ((lambda (x) x) (cons #t #f)))
+; TODO: (display "\n")
+(display ((lambda (x) x) (cons #t (cons #t #f))))
+ (display #t)
+ (display #f)
+; TODO: if demonstration, maybe (if #t (display #t) (display #f))
+; 1
+; 'done
 ; (display 1)
 ; (display '(1 2 3)
 ; (display '(a b c)
 ; TODO: actual closure demonstration
 ; TODO: variable demonstration
-; TODO: if demonstration, maybe (if #t (display #t) (display #f))
 ; TODO: if demonstration with a variable
 ; TODO: loop demonstration
 ;(lambda (x) (tautology (rewrite x) 1 2)) ; Incomplete snippet from boyer
