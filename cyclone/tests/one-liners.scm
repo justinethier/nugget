1b85ee049ad0708e4958f694a77635a5d7a87c97
Justin Ethier <justin.ethier@gmail.com>
2014-10-02 22:00:10 -0400

    New file

---
+++ cyclone/tests/one-liners.scm
@@ -0,0 +1,19 @@
+; One-liners from tmp.scm:
+#t ; Bare minimum 
+(display ((lambda (x) x) (cons #t #f)))
+;; TODO: (display "\n")
+(display ((lambda (x) x) (cons #t (cons #t #f))))
+ (display #t)
+ (display #f)
+(if #t (display #t) (display #f))
+ 1
+; 'done
+(display 1)
+(display (cons (cons 1 2) 3))
+; (display '(1 2 3)
+; (display '(a b c)
+; TODO: actual closure demonstration
+; TODO: variable demonstration
+; TODO: if demonstration with a variable
+; TODO: loop demonstration
+;(lambda (x) (tautology (rewrite x) 1 2)) ; Incomplete snippet from boyer
