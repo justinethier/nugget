ed124c4d9a98df204ff16e6b0056e17a3596bdee
Justin Ethier <justin.ethier@gmail.com>
2014-10-02 22:16:08 -0400

    Removed old code

--- cyclone/trans.scm
+++ cyclone/trans.scm
@@ -963,47 +963,6 @@
  `(lambda ()
     ,(convert exp #f '())))
 
-;; Code emission.
-  
-; c-compile-and-emit : (string -> A) exp -> void
-(define (c-compile-and-emit input-program)
-  (trace:info "---------------- input program:")
-  (trace:info input-program) ;pretty-print
-  
-  (if *do-desugar*
-    (begin
-      (set! input-program (desugar input-program))
-      (trace:info "---------------- after desugar:")
-      (trace:info input-program))) ;pretty-print
-
-  (if *do-cps*
-    (begin
-      (set! input-program (cps-convert input-program))
-      (trace:info "---------------- after CPS:")
-      (trace:info input-program))) ;pretty-print
-
-  (analyze-mutable-variables input-program)
-
-  (set! input-program (desugar (wrap-mutables input-program)))
-  (trace:info "---------------- after wrap-mutables:")
-  (trace:info input-program) ;pretty-print
-
-;; JAE TODO: compare this closure conversion with the one from "90 mins"
-  (set! input-program (closure-convert input-program))
-  (trace:info "---------------- after closure-convert:")
-  (trace:info input-program) ;pretty-print
-  
-  (if (not *do-code-gen*)
-    (begin
-      (trace:error "DEBUG, existing program")
-      (exit)))
-
-  (load "mta/cgen.scm")
-  (load "mta/runtime.scm")
-  (trace:info "---------------- C code:")
-  (mta:code-gen input-program))
-
-
 ;; Compile and emit:
 
 ;;(define the-program
