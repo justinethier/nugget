1f1629c2aeec4edfc8e3b0543fcf48cc71f73748
Justin Ethier <justin.ethier@gmail.com>
2014-10-03 23:49:56 -0400

    Added notes

--- cyclone/tests/if.scm
+++ cyclone/tests/if.scm
@@ -1,3 +1,8 @@
 ;(if #t 1 2)
 (if (+ 1 2) (+ 3 4) (* 3 4))
 (if ((lambda (x) (+ x 1)) 0) (+ 1 1) (* 0 0))
+
+;; TODO: a few problems in compile code, but first priority is GC.
+;; for some reason, after the second major collection, gc_cont is 
+;; never transported, so the pointer becomes invalid. what is
+;; going on there?
