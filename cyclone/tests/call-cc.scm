62ce589fede05f11e1829adf13d6c6b3e192cf48
Justin Ethier <justin.ethier@gmail.com>
2014-10-14 13:16:24 -0400

    WIP

--- cyclone/tests/call-cc.scm
+++ cyclone/tests/call-cc.scm
@@ -1,12 +1,10 @@
 ;; Example of an escape continuation
 (call/cc
-  (lambda (k) ; call it 'return' and things break... uh oh
-              ; real compiler needs to prevent creating identifiers
-              ; that are C keywords
+  (lambda (return)
     (begin
       (display 1)
       (display 2)
-      (k 2)
+      (return 2)
       (display 3)
       (display 4))))
 
