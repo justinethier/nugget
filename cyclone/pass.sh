b08329833a6f33f7e1eacb489c225e1ba32bab7c
Justin Ethier <justin.ethier@gmail.com>
2014-10-13 22:45:31 -0400

    Fixed syntax for huski

--- cyclone/pass.sh
+++ cyclone/pass.sh
@@ -2,7 +2,7 @@
 #echo "(display ((lambda (x) (* x x)) 10))" | huski compiler.scm > 100.c && gcc 100.c -o 100
 
 CC=./compiler
-#CC=huski compiler.scm
+#CC="huski compiler.scm"
 
 if [[ $# -eq 0 ]] ; then
     echo 'usage: pass.sh file.scm'
