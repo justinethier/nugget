85c0ae61f180f034e1c560df4039e5fb9eab205c
Justin Ethier <justin.ethier@gmail.com>
2014-10-14 22:49:52 -0400

    WIP

--- cyclone/examples/tail-call-testing.scm
+++ cyclone/examples/tail-call-testing.scm
@@ -6,6 +6,7 @@
 ;; (define (bar) (foo))
 ;; (foo)
 
+; TODO: this is broken since length is supposed to work on a list, no pair
 (letrec ((foo (lambda (x) (display (length x)) (bar (cons 1 x))))
          (bar (lambda (x) (foo (cons 1 x)))))
     (foo (cons 1 2)))
