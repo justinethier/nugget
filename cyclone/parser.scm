0a09cc3ffbe61eb6e5155cb61a4103ca988742f1
Justin Ethier <justin.ethier@gmail.com>
2014-10-17 14:26:34 -0400

    Initial file

---
+++ cyclone/parser.scm
@@ -0,0 +1,37 @@
+(define (lex fp)
+  (letrec (
+   (->tok (lambda (lst)
+            (list->string ;; TODO: may just be for temp debugging
+              (reverse lst))))
+   (loop/tok (lambda (tok toks comment?)
+                (if (null? tok)
+                    (loop '() toks comment?)
+                    (loop '() (cons (->tok tok) toks) comment?))))
+   (loop (lambda (tok toks comment?)
+    (let ((c (read-char fp)))
+      (cond
+        ((eof-object? c) 
+         (reverse toks))
+        (comment?
+         (if (eq? c #\newline)
+             (loop '() toks #f))
+             (loop '() toks #t))
+        ((char-whitespace? c)
+         (loop/tok tok toks #f))
+        ((eq? c #\;)
+         (loop/tok tok toks #t))
+        ((eq? c #\()
+idea is to form a new list when open paren encountered
+and to end that list upon close paren
+         ;; TODO: need to error if close paren never found
+         (let ((sub (lex fp))
+         (loop '() (cons 'TOK-oparen toks) #f))
+        ((eq? c #\))
+         ;; TODO: what if too many close parens??
+         (loop '() (cons 'TOK-cparen toks) #f))
+        (else
+          (loop (cons c tok) toks #f)))))))
+   (loop '() '() #f)))
+
+(let ((fp (open-input-file "tests/begin.scm")))
+  (write (lex fp)))
