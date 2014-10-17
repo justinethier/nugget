703032a66441b710ac1e06727109c8dd4f70b047
Justin Ethier <justin.ethier@gmail.com>
2014-10-17 11:20:55 -0400

    Added notes

--- cyclone/examples/tail-call-testing.scm
+++ cyclone/examples/tail-call-testing.scm
@@ -1,12 +1,14 @@
-;; This should run forever using a constant amount of memory
-;; and max CPU:
-
-;; Original program:
-;; (define (foo) (bar))
-;; (define (bar) (foo))
-;; (foo)
-
-; TODO: this is broken since length is supposed to work on a list, no pair
-(letrec ((foo (lambda (x) (display (length x)) (bar (cons 1 x))))
+;; A program to use all available memory, and eventually crash
+(letrec ((foo (lambda (x) 
+                (write (length x)) 
+                (bar (cons 1 x))))
          (bar (lambda (x) (foo (cons 1 x)))))
-    (foo (cons 1 2)))
+    (foo '()))
+
+;; TODO: try rewriting it so memory is reclaimed. Does it run
+;; forever now?
+;(letrec ((foo (lambda (x) 
+;                (write (length x)) 
+;                (bar (cons 1 x))))
+;         (bar (lambda (x) (foo (cons 1 x)))))
+;    (foo '()))
