15c7616ec6213e8cae026cfc375a8da69b20a40c
Justin Ethier <justin.ethier@gmail.com>
2014-10-27 22:20:48 -0400

    Changing syntax for now to get it to compile

--- cyclone/tests/fac.scm
+++ cyclone/tests/fac.scm
@@ -1,6 +1,8 @@
-(define (fac n)
+; TODO: once this works, make it work with (define) syntax
+;(define (fac n)
+(set! fac (lambda (n)
     (if (zero? n)
         1
-        (* n (fac (- n 1)))))
+        (* n (fac (- n 1))))))
 (display (fac 10))
 ;(newline)
