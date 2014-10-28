95aed4d05cec2767653cf1ed243a7361416a15a3
Justin Ethier <justin.ethier@gmail.com>
2014-10-28 10:23:55 -0400

    Added conditional

--- cyclone/tests/recursion.scm
+++ cyclone/tests/recursion.scm
@@ -2,5 +2,5 @@
 (letrec ((fnc (lambda (i) 
                 (begin
                     (display i)
-                    (if i (fnc (- i 1)) 0)))))
+                    (if (> i 0) (fnc (- i 1)) 0)))))
     (fnc 10))
