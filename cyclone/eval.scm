383a12422f4da49be235ce1c201839428b74994e
Justin Ethier <justin.ethier@gmail.com>
2014-11-10 17:21:41 -0500

    WIP

--- cyclone/eval.scm
+++ cyclone/eval.scm
@@ -2,41 +2,36 @@
 ;; http://mitpress.mit.edu/sicp/full-text/book/book-Z-H-26.html#%_sec_4.1
 ;;
 
-(define (eval exp ) ;env)
-  (cond ((self-evaluating? exp) exp)
-;        ((variable? exp) (lookup-variable-value exp env))
-;        ((quoted? exp) (text-of-quotation exp))
-;        ((assignment? exp) (eval-assignment exp env))
-;        ((definition? exp) (eval-definition exp env))
-;        ((if? exp) (eval-if exp env))
-;        ((lambda? exp)
-;         (make-procedure (lambda-parameters exp)
-;                         (lambda-body exp)
-;                         env))
-;        ((begin? exp) 
-;         (eval-sequence (begin-actions exp) env))
-;        ((cond? exp) (eval (cond->if exp) env))
-;        ((application? exp)
-;         (apply (eval (operator exp) env)
-;                (list-of-values (operands exp) env)))
-        (else
-         (error "Unknown expression type -- EVAL" exp))))
+(define (eval exp . env)
+  ((analyze exp) env))
+
+(define (self-evaluating? exp)
+  (cond ((number? exp) #t)
+        ;((string? exp) true)
+        (else #f)))
 
 ;; Improvement from section 4.1.7 - Separate syntactic analysis from execution
 (define (analyze exp)
   (cond ((self-evaluating? exp) 
          (analyze-self-evaluating exp))
-        ((quoted? exp) (analyze-quoted exp))
-        ((variable? exp) (analyze-variable exp))
-        ((assignment? exp) (analyze-assignment exp))
-        ((definition? exp) (analyze-definition exp))
-        ((if? exp) (analyze-if exp))
-        ((lambda? exp) (analyze-lambda exp))
-        ((begin? exp) (analyze-sequence (begin-actions exp)))
-        ((cond? exp) (analyze (cond->if exp)))
-        ((application? exp) (analyze-application exp))
+        ;((quoted? exp) (analyze-quoted exp))
+        ;((variable? exp) (analyze-variable exp))
+        ;((assignment? exp) (analyze-assignment exp))
+        ;((definition? exp) (analyze-definition exp))
+        ;((if? exp) (analyze-if exp))
+        ;((lambda? exp) (analyze-lambda exp))
+        ;((begin? exp) (analyze-sequence (begin-actions exp)))
+        ;((cond? exp) (analyze (cond->if exp)))
+        ;((application? exp) (analyze-application exp))
         (else
          (error "Unknown expression type -- ANALYZE" exp))))
+
+(define (analyze-self-evaluating exp)
+  (lambda (env) exp))
+
+;(define (analyze-quoted exp)
+;  (let ((qval (text-of-quotation exp)))
+;    (lambda (env) qval))
 
 ;; JAE - Testing
 (write (eval 2))
