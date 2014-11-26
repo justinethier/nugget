36db58beaa5d5201c626abd1dd3b56aaea94708f
Justin Ethier <justin.ethier@gmail.com>
2014-11-26 13:14:31 -0500

    Working through issues with first-class functions

--- cyclone/map.scm
+++ cyclone/map.scm
@@ -1,9 +1,11 @@
 ;; TODO: relocate this stuff into a common place, and delete this file
-(define (foldr func end lst)
-  (if (null? lst)
-      end
-          (func (car lst) (foldr func end (cdr lst)))))
-
-(define (map func lst)        (foldr (lambda (x y) (cons (func x) y)) '() lst))
-
-(write (map car '((a . b) (1 . 2) (#\h #\w))))
+(define a cons)
+(write (a 1 2))
+;(define (foldr func end lst)
+;  (if (null? lst)
+;      end
+;          (func (car lst) (foldr func end (cdr lst)))))
+;
+;(define (map func lst)        (foldr (lambda (x y) (cons (func x) y)) '() lst))
+;
+;(write (map car '((a . b) (1 . 2) (#\h #\w))))
