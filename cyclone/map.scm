872090c435e67fb6074a16f16ac9e7e312982819
Justin Ethier <justin.ethier@gmail.com>
2014-11-25 22:55:39 -0500

    Adding test file

---
+++ cyclone/map.scm
@@ -0,0 +1,9 @@
+;; TODO: relocate this stuff into a common place, and delete this file
+(define (foldr func end lst)
+  (if (null? lst)
+      end
+          (func (car lst) (foldr func end (cdr lst)))))
+
+(define (map func lst)        (foldr (lambda (x y) (cons (func x) y)) '() lst))
+
+(write (map car '((a . b) (1 . 2) (#\h #\w))))
