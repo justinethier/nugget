8720f3ad0a6c9f7c82170aa538a0e27c83e11de2
Justin Ethier <justin.ethier@gmail.com>
2014-12-03 23:00:00 -0500

    Added breaking test case - no else clause

--- cyclone/tests/if.scm
+++ cyclone/tests/if.scm
@@ -2,6 +2,7 @@
 (display (if #f 1 2))
 (if (+ 1 2) (+ 3 4) (* 3 4))
 (if ((lambda (x) (+ x 1)) 0) (+ 1 1) (* 0 0))
+(write (if #t 'then-clause))
 
 ;; TODO: a few problems in compile code, but first priority is GC.
 ;; for some reason, after the second major collection, gc_cont is 
