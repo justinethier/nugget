14e54813250439d5fc36166a865db4286fb57d05
Justin Ethier <justin.ethier@gmail.com>
2014-12-10 13:04:15 -0500

    Added vararg unit tests

--- cyclone/tests/apply.scm
+++ cyclone/tests/apply.scm
@@ -1,3 +1,11 @@
 (write (apply length '((#t #f))))
 (write (apply cons '(#t #f)))
 ;(write (apply + '(10 20))) ; may need to change representation of symbols to make this work
+
+;; Varargs
+(define (list . objs)  objs)
+(define (list2 a b . objs) objs)
+(write (list 42 1))
+(write (list 42 1 2))
+(write (list2 42 1))
+(write (list2 42 1 2))
