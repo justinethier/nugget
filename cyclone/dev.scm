ba92e819c2601e3c55309d69aae223b812b7eca1
Justin Ethier <justin.ethier@gmail.com>
2014-12-20 23:30:04 -0500

    Added more notes

--- cyclone/dev.scm
+++ cyclone/dev.scm
@@ -2,6 +2,13 @@
 ;; supported yet by cyclone
 ;;
 
+;; TODO: write these char functions as lisp built-ins
+;;       BUT, let's change that code to scan for built-ins, and
+;;       only include those that are actually used. may want to just
+;;       make a single scan across the program for all fv's, and then
+;;       reconcile that list with built-ins, and only include those that
+;;       might be used. IE, any fv == map, include the map built-in.
+
 (char-whitespace? #\space)
 (char-whitespace? #\a)
 (char-numeric? #\1)
@@ -9,10 +16,12 @@
 ;(eof-object?)
 ;(read-char fp)
 ;list->string
+; reverse
+;string-append
 ;string->number
 ;string->symbol - TODO: can runtime allow dynamically-created symbols?
 ; letrec - TBD, may just restructure parser code to use defines
-;open-input-file
+;open-input-file (and closing too, I suppose)
 ; read - can this be derived from parser.scm implementation???
 
 ; idea - booleans
