d9dfb96d9309dca4ae9ca36ef2ba0864e9cefa69
Justin Ethier <justin.ethier@gmail.com>
2014-12-22 22:41:36 -0500

    Added more tests

--- cyclone/tests/unit-tests.scm
+++ cyclone/tests/unit-tests.scm
@@ -1,16 +1,16 @@
 (define (assert msg actual expected)
   (if (not (equal? actual expected))
-      (error "Unit test failed " msg " actual " actual " expected " expected "")))
-      ; Parser issues here:
-      ;(error "Unit test failed [" msg "] actual [" actual "] expected [" expected "]")))
+      (error "Unit test failed [" msg "] actual [" actual "] expected [" expected "]")))
 
-(assert "small reverse" (reverse '(a b)) '(b a))
-(assert "" #t #f)
-;;  (reverse '(a b c))
-;;  (reverse '(1 2 3 4 5 6 7 8 9 10))
+; TODO: this should pass:
+;(assert "numeric small reverse" (reverse '(1 2)) '(2 1))
+(assert "small reverse" (reverse '(a b c)) '(c b a))
+;TODO: same as above -(assert "larger reverse" (reverse '((1 2 3 4 5 6 7 8 9 10))) '(10 9 8 7 6 5 4 3 2 1))
 ;;  ;TODO: improper list, this is an error: (reverse '(1 . 2))
-;;  (char-whitespace? #\space)
-;;  (char-whitespace? #\a)
-;;  (char-numeric? #\1)
-;;  (char-numeric? #\newline)))
+(assert "char whitespace" (char-whitespace? #\space) #t)
+(assert "char whitespace" (char-whitespace? #\a) #f)
+(assert "char numeric" (char-numeric? #\1) #t)
+(assert "char numeric" (char-numeric? #\newline) #f)
+; TODO: use display, output without surrounding quotes
+(write "All test passed")
 ;;
