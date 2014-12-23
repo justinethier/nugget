d88a1cae0abc92b0cacd3776363ea9c906c468ea
Justin Ethier <justin.ethier@gmail.com>
2014-12-22 22:05:04 -0500

    Debugging issues with (error)

--- cyclone/tests/unit-tests.scm
+++ cyclone/tests/unit-tests.scm
@@ -2,7 +2,8 @@
   (if (not (equal? actual expected))
       (error "Unit test failed [" msg "] actual [" actual "] expected [" expected "]")))
 
-(assert "small reverse" (reverse '(1 2)) '(2 1))
+(assert "small reverse" (reverse '(a b)) '(b a))
+(assert "" #t #f)
 ;;  (reverse '(a b c))
 ;;  (reverse '(1 2 3 4 5 6 7 8 9 10))
 ;;  ;TODO: improper list, this is an error: (reverse '(1 . 2))
