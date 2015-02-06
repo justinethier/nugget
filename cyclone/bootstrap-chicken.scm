6055477ada0853c96602168ae80791aed1ee22ad
Justin Ethier <justin.ethier@gmail.com>
2015-02-05 22:46:03 -0500

    Removed unused function

--- cyclone/bootstrap-chicken.scm
+++ cyclone/bootstrap-chicken.scm
@@ -1,19 +1,3 @@
 ;; CHICKEN imports
 (require-extension extras) ;; pretty-print
 (require-extension chicken-syntax) ;; when
-
-;; Read all s-expressions from file
-(define (read-all)
-  (letrec ((read-next-sexp 
-    (lambda (result)
-      (let ((obj (read)))
-        (if (eof-object? obj)
-            ;(begin
-            ;  (close-input-port fp)
-            ;  result)
-            result
-            (read-next-sexp 
-              (cons obj result)))))))
-    (reverse
-      (read-next-sexp (list)))))
-
