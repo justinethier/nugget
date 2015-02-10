018652aafe7be86bed3717d8257b94ca54c6ce97
Justin Ethier <justin.ethier@gmail.com>
2015-02-10 13:10:01 -0500

    Added notes

--- cyclone/test.scm
+++ cyclone/test.scm
@@ -1,5 +1,21 @@
-;; A temporary test file, delete this once it works!
+;; A temporary test file, move everything to a test suite and/or docs once it works!
 
+;; The purpose of this file is to test interactions between the interpreter
+;; and compiled code.
+;;
+;; Some requirements and notes:
+;; - The interpreter can create new variables, but those new vars are only 
+;;   accessible by the interpreter (otherwise code would not compile)
+;; - The interpreter should be able to access compiled variables, including
+;;   functions
+;; - The interpreter should be able to change variables that originate
+;;   in compiled code
+;; - If eval is never called, compiled code can be more efficient by omitting
+;;   information that is only required by the interpreter.
+;; 
+;; How to represent environments? 
+;; Presumably the interpreter's global environment needs to include compiled globals as well. It should also be extended to include local vars, presumably prior to each call to eval??
+;; 
 ;; case #1 - pass a global variable to the interpreter
 (define x 1)
 (write (eval 'x))
