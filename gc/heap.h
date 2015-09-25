5cd05183c63c1d063106f855839c785304bc2c81
Justin Ethier <justin.ethier@gmail.com>
2015-09-24 22:35:05 -0400

    WIP

--- gc/heap.h
+++ gc/heap.h
@@ -1,6 +1,11 @@
 #ifndef GC_HEAP_H
 #define GC_HEAP_H
 
+/*
+notes:
+per DLG-successor paper, could have a bin tree (??) of free blocks. tree starts out with a single node of one free block
+*/
+
 const int pagesize = 1024; //4096 bytes?
 typedef struct {
 } heap_header_type;
--- gc/main.c
+++ gc/main.c
@@ -1,5 +1,8 @@
 // TODO: a basic mark-sweep GC
 
+typedef struct {
+  unsigned char mark;
+} gc_header_type;
 #include "types.h"
 
 int main(int argc, char **argv) {
