f69c5996e4086b2e03d1d1559da2024ade2d5fac
Justin Ethier <justin.ethier@gmail.com>
2015-09-30 23:11:15 -0400

    WIP - heap

--- gc/main.c
+++ gc/main.c
@@ -1,5 +1,48 @@
 // TODO: a basic mark-sweep GC
 
+
+// HEAP definitions
+// experimenting with a heap based off of the one in Chibi scheme
+#define gc_free_chunk_size (sizeof(gc_free_list))
+typedef struct {
+  uint size;
+  gc_free_list *next;
+} gc_free_list;
+
+typedef struct {
+  uint size, chunk_size
+  gc_free_list *free_list; // TBD
+  gc_heap_block *next; // TBD, linked list is not very efficient, but easy to work with as a start
+  char *data;
+} gc_heap_block;
+
+gc_heap_block *gc_heap_create(size_t size, size_t chunk_size)
+{
+  gc_free_list *free, *next;
+  gc_heap_block *h;
+
+  // TODO: mmap?
+  h = malloc(size);
+  if (!h) return NULL;
+
+  h->size = size;
+  h->chunk_size = chunk_size;
+  h->data = (char *)&(h->data); 
+  h->next = NULL;
+  free = h->free_list = (gc_free_list *)h->data;
+  next = (gc_free_list *)(((char *) free) + 
+  free->size = 0;
+  free->next = 
+  // TODO: free, next
+
+  return h;
+}
+
+void gc_init()
+{
+}
+// END heap definitions
+
 typedef struct {
   unsigned char mark; // mark bits (only need 2)
   // TODO: forwarding address (probably not needed for mark/sweep), anything else???
