9ed823368dc61788a869ebfdd130a5ae8c9e207d
Justin Ethier <justin.ethier@gmail.com>
2015-10-02 01:24:12 -0400

    Heap/word alignment testing

--- gc/align.c
+++ gc/align.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
-#define sexp_align(n, bits) (((n)+(1<<(bits))-1)&(((unsigned int)-1)-((1<<(bits))-1)))
-#define sexp_word_align(n) sexp_align((n), 2)
+#define gc_align(n, bits) (((n)+(1<<(bits))-1)&(((unsigned int)-1)-((1<<(bits))-1)))
+// 64-bit is 3, 32-bit is 2
+#define gc_word_align(n) gc_align((n), 2)
+#define gc_heap_align(n) gc_align(n, 5)
 int main(){
-  printf("%d\n", sexp_word_align(1));
-  printf("%d\n", sexp_word_align(2));
-  printf("%d\n", sexp_word_align(3));
+  printf("%d\n", gc_word_align(1));
+  printf("%d\n", gc_word_align(2));
+  printf("%d\n", gc_word_align(3));
+
+  printf("%d\n", gc_align(1, 4));
+  printf("%d\n", gc_align(1, 5));
   return 0;
 }
